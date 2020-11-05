9667ea2e6c98732608d4334c1a28f1ae6ce334dd
Lukas van Driel <lukas@q42.nl>
2020-11-05 15:26:23 +0100

    save to monster events

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -4,8 +4,19 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.addMessage = functions.https.onRequest(async (req, res) => {
-  const text = req.query.text;
-  const doc = { text, date: (+new Date()) };
-  const writeResult = await admin.firestore().collection('messages').add(doc);
-  res.json({ id: `/messages/${writeResult.id}`, doc });
+  const body = req.body;
+  if (typeof body !== "object") {
+    return res.status(400).json({ message: "body is not an object, content-type:application/json missing?" });
+  }
+
+  const user = body.user;
+  const doc = { body, timestamp: (+new Date()) };
+
+  if (!user) {
+    return res.status(400).json({ message: "missing property 'user' containing userid (=monsterid)" });
+  }
+
+  console.log(`pushing to /monsters/${user}/events: ${JSON.stringify(doc)}`);
+  const writeResult = await admin.firestore().collection("monsters").doc(user).collection('events').add(doc);
+  res.json({ id: `/monsters/${user}/events/${writeResult.id}`, doc });
 });
