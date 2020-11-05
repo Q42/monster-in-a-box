ac035a066485d46e0008bf7c6dcb38b0346868d1
Arian van Gend <arian@q42.nl>
2020-11-05 13:27:37 +0000

    First rough listen to events of the monster

--- pi/js/app.js
+++ pi/js/app.js
@@ -1,7 +1,10 @@
 const http = require('http');
 // var GrovePi = require('node-grovepi').GrovePi
-var player = require('play-sound')(opts = { player: 'mpg123' });
+//var player = require('play-sound')(opts = { player: 'mpg123' });
 // https://github.com/shime/play-sound
+
+var admin = require('firebase-admin');
+var serviceAccount = require("./serviceaccount.json");
 
 const PORT = 3000;
 
@@ -38,4 +41,27 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}.`);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://monster-in-a-box.firebaseio.com'
+  });
+
+  const db = admin.firestore();
+
+  // TODO: insert doc in monsters collection.
+  
+  // TODO: don't hardcode monster ID.
+  const monster = db.collection('monsters').doc('XifqFglx6OzCOmm8wgYa').collection('events');
+
+  const observer = monster.onSnapshot(event => {
+    if (event.empty) {
+      console.log(`No event received`);
+      return;
+    }
+    
+    console.log(`Received monster snapshot: ${JSON.stringify(event.docs[0].data())}`);
+  }, err => {
+    console.log(`Encountered error: ${err}`);
+  });
 });
