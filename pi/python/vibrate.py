f133bed9d2dfd4e9b11116329a3b1daafb0562ed
korjan <korjanvanwieringen@gmail.com>
2020-11-05 09:29:22 +0000

    Initial monster

---
+++ pi/python/vibrate.py
@@ -0,0 +1,26 @@
+import time
+import grovepi
+
+# Connect the Grove Vibration Motor to digital port D8
+# SIG,NC,VCC,GND
+vibration_motor = 2
+
+grovepi.pinMode(vibration_motor,"OUTPUT")
+
+while True:
+    try:
+        # Start vibrating for 1 second
+        grovepi.digitalWrite(vibration_motor,1)
+        print ('start')
+        time.sleep(1)
+
+        # Stop vibrating for 1 second, then repeat
+        grovepi.digitalWrite(vibration_motor,0)
+        print ('stop')
+        time.sleep(1)
+
+    except KeyboardInterrupt:
+        grovepi.digitalWrite(vibration_motor,0)
+        break
+    except IOError:
+        print ("Error")
